0f984cb1eec0b017a826fbb763d010d63bc7b887
wangwenwen <peterven>
2020-03-10 21:53:15 +0800

    update

---
+++ 04_变量的使用.py
@@ -0,0 +1,19 @@
+qq_number = 545
+print(qq_number + 1)
+
+'''
+超市买水果
+'''
+price = 8.5
+weight = 7.5
+money = price * weight
+print("应该要付" + str(money))
+
+# 变量类型
+name = "小明"  # str
+age = 18  # int
+gender = True  # boolean
+height = 1.75  # float
+
+
+
